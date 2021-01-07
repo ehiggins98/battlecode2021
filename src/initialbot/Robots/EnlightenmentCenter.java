cab8666d369dc3e6fcda904663ed850bebe250dc
Ryan Pope <poperyan73@gmail.com>
2021-01-07 12:30:30 -0600

    Update logic to check number of votes

--- src/initialbot/Robots/EnlightenmentCenter.java
+++ src/initialbot/Robots/EnlightenmentCenter.java
@@ -2,14 +2,16 @@ package initialbot.Robots;
 
 import battlecode.common.Direction;
 import battlecode.common.GameActionException;
+import battlecode.common.GameConstants;
 import battlecode.common.RobotController;
 import battlecode.common.RobotType;
 
 public class EnlightenmentCenter implements RobotInterface {
     private final RobotController rc;
 
-    private static int lastInfluence = 150;
+    // private static int lastInfluence = GameConstants.INITIAL_ENLIGHTENMENT_CENTER_INFLUENCE;
     private static int bidValue = 1;
+    private static int lastVoteCount = 0;
     private final int adder = 1;
     private final double multiplier = 1.5;
 
@@ -19,28 +21,27 @@ public class EnlightenmentCenter implements RobotInterface {
 
     @Override
     public void runTurn(int turn) throws GameActionException {
-        int influence = this.rc.getInfluence();
-
-        System.out.println(influence);
-
-        if (influence == lastInfluence - bidValue) {
+        if (this.rc.getTeamVotes() > lastVoteCount) {
             // We won the vote
             System.out.println("We won the vote");
             if (bidValue > adder) {
                 bidValue -= adder;
             }
-        } else if (influence == lastInfluence - Math.ceil(bidValue / 2.0) && turn > 1) {
+        } else if (this.rc.getTeamVotes() == lastVoteCount) {
             // We lost the vote
             bidValue = (int) Math.ceil(bidValue * multiplier);
         }
 
-        if (turn % 100 == 0) {
+        int influence = this.rc.getInfluence();
+        System.out.println(influence);
+
+        if (turn % 100 == 0 && influence >= 20) {
             rc.buildRobot(RobotType.POLITICIAN, Direction.EAST, 20);
         }
 
-        lastInfluence = this.rc.getInfluence();
+        bidValue = Math.min(influence / 4, bidValue);
 
-        bidValue = Math.min(lastInfluence / 4, bidValue);
+        lastVoteCount = this.rc.getTeamVotes();
 
         if (this.rc.canBid(bidValue)) {
             this.rc.bid(bidValue);

839a5979ca9d5f6ff765d2960205ba1878db8dd3
Eric Higgins <ericdhiggins@gmail.com>
2021-01-10 18:27:21 -0600

    Only speech ECs and muckrakers if adjacent

--- src/initialbot/Robots/Politician.java
+++ src/initialbot/Robots/Politician.java
@@ -5,6 +5,7 @@ import battlecode.common.GameActionException;
 import battlecode.common.MapLocation;
 import battlecode.common.RobotController;
 import battlecode.common.RobotInfo;
+import battlecode.common.RobotType;
 import initialbot.MobileRobot;
 import initialbot.Communication.Messages.DefenseLocationMessage;
 
@@ -23,17 +24,9 @@ public class Politician extends MobileRobot {
     public void runTurn(int turn) throws GameActionException {
         super.runTurn(turn);
 
-        MapLocation closestEnemy = getClosestEnemyRobotLocation();
+        RobotInfo closestEnemy = getClosestEnemyRobot();
         if (closestEnemy != null) {
-            int distSquared = rc.getLocation().distanceSquaredTo(closestEnemy);
-            if (distSquared <= maxEmpowerRadiusSquared && rc.canEmpower(distSquared)) {
-                rc.empower(distSquared);
-            } else {
-                Direction step = pathFinder.getStepTowardGoal(closestEnemy);
-                if (step != Direction.CENTER && rc.canMove(step)) {
-                    rc.move(step);
-                }
-            }
+            attackRobot(closestEnemy);
         } else if (lastMessage != null && lastMessage.getClass().equals(DefenseLocationMessage.class)) {
             DefenseLocationMessage dlm = (DefenseLocationMessage) lastMessage;
             MapLocation goal = pathFinder.getLocationAtRadius(ecLocation, dlm.getRadius(), dlm.getDirection());
@@ -44,8 +37,23 @@ public class Politician extends MobileRobot {
         }
     }
 
-    private MapLocation getClosestEnemyRobotLocation() {
-        MapLocation closest = null;
+    private void attackRobot(RobotInfo robot) throws GameActionException {
+        MapLocation current = rc.getLocation();
+        int distSquared = current.distanceSquaredTo(robot.location);
+        if (rc.canEmpower(distSquared) && 
+                (current.isAdjacentTo(robot.location) ||
+                (robot.type.equals(RobotType.POLITICIAN) && distSquared <= maxEmpowerRadiusSquared))) {
+            rc.empower(distSquared);
+        } else {
+            Direction step = pathFinder.getStepTowardGoal(robot.location);
+            if (step != Direction.CENTER && rc.canMove(step)) {
+                rc.move(step);
+            }
+        }
+    }
+
+    private RobotInfo getClosestEnemyRobot() {
+        RobotInfo closest = null;
         int closestDistSquared = Integer.MAX_VALUE;
         MapLocation current = rc.getLocation();
 
@@ -53,7 +61,7 @@ public class Politician extends MobileRobot {
             if (!info.team.equals(rc.getTeam())) {
                 int dist = current.distanceSquaredTo(info.location);
                 if (dist < closestDistSquared) {
-                    closest = info.location;
+                    closest = info;
                     closestDistSquared = dist;
                 }
             }
